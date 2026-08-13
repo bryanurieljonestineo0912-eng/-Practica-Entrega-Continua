c65a511bd424514fb2705a4328f53b1a30ca1e68
Bryan Jones <bryanurieljonestineo0912@gmail.com>
2026-08-12 23:25:50 -0400

    Migrate from HTTP to Express server

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const http = require('http');
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hola Mundo DevOps');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hola Mundo desde Docker y DevOps");
 });
 
-server.listen(3000, () => {
-    console.log('Servidor iniciado en puerto 3000');
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
